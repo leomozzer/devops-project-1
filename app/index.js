cc13a9a88f7175ec1e9fedfe8434ef88ff707c8c
leomozzer <mozzer.ferraz@gmail.com>
2022-09-04 15:49:08 -0300

    Added db connection

---
+++ app/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+require('dotenv').config()
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ 'extended': true }))
+
+app.use((req, res, next) => {
+    next();
+});
+
+app.get('/', (req, res) => {
+    return res.json({
+        "message": `Hello World! ${Date()}`,
+        "host": process.env.MYSQL_HOST
+    })
+})
+
+app.use(require('./routes/database'));
+
+
+app.listen(process.env.APP_PORT, () => {
+    console.log(`Listening on port ${process.env.APP_PORT}`);
+    console.log(`Mysql host: ${process.env.MYSQL_HOST}`)
+})
