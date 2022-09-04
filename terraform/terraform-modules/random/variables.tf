267251da54bb1482ae3e55dc97c68adef617d438
leomozzer <mozzer.ferraz@gmail.com>
2022-09-04 11:52:20 -0300

    Added random module

---
+++ terraform/terraform-modules/random/variables.tf
@@ -0,0 +1,19 @@
+variable "length" {
+  type    = number
+  default = 5
+}
+
+variable "special" {
+  type    = bool
+  default = false
+}
+
+variable "upper" {
+  type    = bool
+  default = false
+}
+
+variable "numeric" {
+  type    = bool
+  default = false
+}
