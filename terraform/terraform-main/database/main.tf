203f006dfaac8dec718834e91c0ff3603ef4a6bf
leomozzer <mozzer.ferraz@gmail.com>
2022-09-07 18:33:18 -0300

    Nodejs app deployment

--- terraform/terraform-main/database/main.tf
+++ terraform/terraform-main/database/main.tf
@@ -84,14 +84,14 @@ resource "azurerm_linux_web_app" "webapp" {
   # app_settings = {
   #   "WEBSITE_RUN_FROM_PACKAGE" = 1
   # }
-  #zip_deploy_file = "../../../app/app.zip"
+  # zip_deploy_file = "../../../app/app.zip"
 }
 
 #Deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id   = azurerm_linux_web_app.webapp.id
-  repo_url = "https://github.com/leomozzer/devops-project-1"
-  branch   = "main"
+  repo_url = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+  branch   = "master"
 
   use_manual_integration = true
   use_mercurial          = false
